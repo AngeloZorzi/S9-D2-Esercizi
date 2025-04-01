35205dc7ee590a5885959270622fa567e9879167
AngeloZorzi <astro.drg666@gmail.com>
2025-04-01 16:44:11 +0200

    updates components and css

--- src/MyFooter.jsx
+++ src/MyFooter.jsx
@@ -1,14 +1,17 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 const MyFooter = function () {
+  const currentYear = new Date().getFullYear();
+
   return (
     <>
       <Container fluid className="p-0">
-        <footer className="bg-dark text-light">
-          <p>La Magica Libreria</p>
+        <footer className="bg-dark text-light text-center py-3">
+          <p>La Magica Libreria - {currentYear}</p>
         </footer>
       </Container>
     </>
   );
 };
+
 export default MyFooter;
