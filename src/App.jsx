e9b9d2b7b67a0e9161300474bb04dc1256103fd7
AngeloZorzi <astro.drg666@gmail.com>
2025-04-01 16:21:21 +0200

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MyNavbar from "./CustomNavbar";
 import MyFooter from "./MyFooter";
+import Welcome from "./Welcome";
 
 function App() {
   return (
     <>
       <body className="vh-100">
         <MyNavbar />
+        <Welcome />
 
         <MyFooter />
       </body>
